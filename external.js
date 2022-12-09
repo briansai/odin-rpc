0ccc31c6057ed81cabdf8f76c44ac02bc569cf7c
Brian Sai <brianwsai@gmail.com>
2022-12-08 18:42:33 -0800

    Implemented button clicking logic

--- external.js
+++ external.js
@@ -1,4 +1,36 @@
+const getPlayerChoice = (e) => {
+  const option = e.target.textContent.toLowerCase();
+  num = compare(option, getComputerChoice());
+  console.log(num);
+  if (num) {
+    competitors[num]++;
+    rounds--;
+    num = 0;
+  }
+
+  if (rounds === 0) {
+    if (competitors[1] > competitors[2]) {
+      console.log('Player 1 wins!');
+      return 'Player 1 wins!';
+    }
+
+    console.log('Computer wins!');
+    return 'Computer wins!';
+  }
+};
+
+document
+  .querySelectorAll('button')
+  .forEach(() => addEventListener('click', getPlayerChoice));
+
 const options = ['rock', 'paper', 'scissors'];
+
+let num = 0;
+let rounds = 5;
+const competitors = {
+  1: 0,
+  2: 0,
+};
 
 const getRandomInt = () => Math.floor(Math.random() * 3);
 
@@ -19,32 +51,3 @@ const compare = (player, computer) => {
 
   return 0;
 };
-
-const playRound = () => {
-  let num = 0;
-  let rounds = 5;
-  const competitors = {
-    1: 0,
-    2: 0,
-  };
-
-  while (rounds || (!num && rounds)) {
-    num = compare(prompt('Enter your choice'), getComputerChoice());
-
-    if (num) {
-      competitors[num]++;
-      rounds--;
-      num = 0;
-    }
-  }
-
-  if (competitors[1] > competitors[2]) {
-    console.log('Player 1 wins!');
-    return 'Player 1 wins!';
-  }
-
-  console.log('Computer wins!');
-  return 'Computer wins!';
-};
-
-playRound();
