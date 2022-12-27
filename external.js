78920575daa5187d991945042267eea6103c92c2
Brian Sai <brianwsai@gmail.com>
2022-12-26 18:40:55 -0800

    Implemented logic to better handle the options

--- external.js
+++ external.js
@@ -1,64 +1,61 @@
-const runGame = () => {
-  let num = 0;
-  let rounds = 5;
-  let winner = null;
-  const competitors = {
-    1: 0,
-    2: 0,
-  };
-
-  const getPlayerChoice = (e) => {
-    const option = e.target.textContent.toLowerCase();
-    num = compare(option, getComputerChoice());
-
-    if (num) {
-      competitors[num]++;
-      rounds--;
-      num = 0;
-    }
-
-    if (rounds === 0) {
-      if (competitors[1] > competitors[2]) {
-        winner = 1;
-      }
-
-      winner = 2;
-    }
-
-    if (winner) {
-      document.querySelector('.header').textContent = `Player ${winner} wins!`;
-      document
-        .querySelectorAll('button')
-        .forEach((btn) => (btn.disabled = true));
-      return winner;
-    }
-  };
-
-  document
-    .querySelectorAll('button')
-    .forEach(() => addEventListener('click', getPlayerChoice));
-
-  const options = ['rock', 'paper', 'scissor'];
-
-  const getRandomInt = () => Math.floor(Math.random() * 3);
-
-  const getComputerChoice = () => options[getRandomInt()];
-
-  const compare = (player, computer) => {
-    const winner = {
-      rock: 'scissor',
-      paper: 'rock',
-      scissor: 'paper',
-    };
-
-    if (winner[player] === computer) {
-      return 1;
-    } else if (winner[computer] === player) {
-      return 2;
-    }
-
-    return 0;
-  };
+let num = 0;
+let rounds = 5;
+let winner = null;
+let option = null;
+const competitors = {
+  1: 0,
+  2: 0,
 };
 
-runGame();
+const getPlayerChoice = (e) => {
+  option = e.target.classList[1];
+  num = compare(option, getComputerChoice());
+  console.log(e);
+  if (num) {
+    competitors[num]++;
+    rounds--;
+    num = 0;
+  }
+
+  if (rounds === 0) {
+    if (competitors[1] > competitors[2]) {
+      winner = 1;
+    }
+
+    winner = 2;
+  }
+  // btn disabled, but its children are still clickable
+  if (winner) {
+    document.querySelector('.header').textContent = `Player ${winner} wins!`;
+    document.querySelectorAll('button').forEach((btn) => (btn.disabled = true));
+    console.log(winner);
+    return winner;
+  }
+};
+
+document
+  .querySelectorAll('.btn')
+  .forEach(() => addEventListener('click', getPlayerChoice));
+
+const options = ['rock', 'paper', 'scissor'];
+
+const getRandomInt = () => Math.floor(Math.random() * 3);
+
+const getComputerChoice = () => options[getRandomInt()];
+
+const compare = (player, computer) => {
+  const winner = {
+    rock: 'scissor',
+    paper: 'rock',
+    scissor: 'paper',
+  };
+  console.log('player-->', player);
+  console.log('computer---->', computer);
+  if (winner[player] === computer) {
+    return 1;
+  } else if (winner[computer] === player) {
+    return 2;
+  }
+
+  return 0;
+};
