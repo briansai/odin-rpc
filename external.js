3c68b6724d99c4ca7122e169de662fba8bad3e59
Brian Sai <brianwsai@gmail.com>
2022-12-28 16:59:46 -0800

    Implemented replay option and buttons

--- external.js
+++ external.js
@@ -2,7 +2,7 @@ let num = 0;
 let rounds = 5;
 let winner = null;
 let option = null;
-let setReplay = false;
+let replayOptionClicked = false;
 const competitors = {
   1: 0,
   2: 0,
@@ -30,29 +30,58 @@ const getPlayerChoice = (e) => {
     winner = 2;
   }
 
-  if (winner && !setReplay) {
+  if (winner) {
+    let headerContainer = document.querySelector('.header-container');
+    let buttons = document.querySelector('.buttons');
+
+    let replayContainer = document.createElement('div');
+    replayContainer.className = 'replay-container';
+
     let header = document.querySelector('.header');
+    header.textContent = `Player ${winner} wins!`;
+
     let replay = document.createElement('h2');
     replay.textContent = 'Do you want to play again?';
-    header.textContent = `Player ${winner} wins!`;
-    document.querySelector('.header-container').append(replay);
 
-    document.querySelector('.buttons').remove();
+    let replayOptionYes = document.createElement('button');
+    replayOptionYes.textContent = 'Yes';
+    replayOptionYes.addEventListener('click', () => {
+      replayOptionClicked = true;
+      window.location.reload();
+    });
 
-    setReplay = true;
+    let replayOptionNo = document.createElement('button');
+    replayOptionNo.textContent = 'No';
+    replayOptionNo.addEventListener('click', () => {
+      replayOptionClicked = true;
+      // create something here when click is no.
+    });
+
+    if (!replayOptionClicked) {
+      replayContainer.append(replay);
+      replayContainer.append(replayOptionYes);
+      replayContainer.append(replayOptionNo);
+      headerContainer.append(replayContainer);
+    }
+
+    buttons && buttons.remove();
+
     return winner;
   }
 };
+const options = [
+  { text: 'rock', sign: '✊' },
+  { text: 'paper', sign: '✋' },
+  { text: 'scissor', sign: '✌' },
+];
 
 document
   .querySelectorAll('.btn')
   .forEach(() => addEventListener('click', getPlayerChoice));
 
-const options = ['rock', 'paper', 'scissor'];
-
 const getRandomInt = () => Math.floor(Math.random() * 3);
 
-const getComputerChoice = () => options[getRandomInt()];
+const getComputerChoice = () => options[getRandomInt()].text;
 
 const compare = (player, computer) => {
   const winner = {
